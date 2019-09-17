d57d098d43424cf3c2bd5280fe7dd6307d843987
Álvaro Belmonte <abelmonte4eso2@gmail.com>
2019-09-17 18:54:49 +0200

    changed port

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ app.use('/api/auth', require('./routes/auth'));
 
 
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log('Server started on port ' + PORT));
